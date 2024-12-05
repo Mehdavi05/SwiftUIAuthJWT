d7f8f34ee3e5614549d945949f8974167bc850d4
Shujaat Hussain <shujaat.apple05@gmail.com>
2024-12-05 17:00:39 +0400

    Improved Login Screen UI

--- SwiftClient/LoginView.swift
+++ SwiftClient/LoginView.swift
@@ -12,29 +12,50 @@ struct LoginView: View {
     @ObservedObject var accountListVM = AccountListViewModel()
     
     var body: some View {
-        VStack {
-            Form {
-                HStack {
-                    Spacer()
-                    Image(systemName: loginVM.isAuthenticated ? "lock.fill": "lock.open")
-                }
-                TextField("Username", text: $loginVM.username)
-                SecureField("Password", text: $loginVM.password)
-                HStack {
-                    Spacer()
-                    Button("Login") {
-                        loginVM.login()
+        GeometryReader { geometry in
+            VStack {
+                Form {
+                    HStack {
+                        Spacer()
+                        Image(systemName: loginVM.isAuthenticated ? "lock.fill" : "lock.open")
+                            .font(.title)
+                            .foregroundColor(.teal)
                     }
-                    Button("Signout") {
-                        loginVM.signout()
+                    
+                    TextField("Username", text: $loginVM.username)
+                    SecureField("Password", text: $loginVM.password)
+                    
+                    HStack {
+                        Spacer()
+                        Button("Login") {
+                            loginVM.login()
+                        }
+                        .padding()
+                        .background(Color.teal)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        
+                        Button("Signout") {
+                            loginVM.signout()
+                        }
+                        .padding()
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        Spacer()
                     }
-                    Spacer()
                 }
-            }.buttonStyle(PlainButtonStyle())
-        } .onAppear(perform: {
-            
-        })
+                .buttonStyle(PlainButtonStyle())
+                .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.34) // Adjust size of the form
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(16)
+                .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 5)
+                .frame(maxWidth: .infinity, maxHeight: .infinity) // Center Form
+            }
+        }
         .navigationTitle("Login")
+        .navigationBarTitleDisplayMode(.inline)
+        .ignoresSafeArea(edges: .top)
         .embedInNavigationView()
     }
 }
